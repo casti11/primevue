5cd2082d84a9ec76d25115a3edd429b6e57d4952
Guille Castillo <37068618+casti11@users.noreply.github.com>
2019-11-06 09:38:29 +0100

    Changed prop readonly to readOnly
    The prop readonly was later incorrectly referenced as this.readOnly, causing the read only option to not work.

--- src/components/editor/Editor.vue
+++ src/components/editor/Editor.vue
@@ -54,7 +54,7 @@ export default {
     props: {
         value: String,
         placeholder: String,
-        readonly: Boolean,
+        readOnly: Boolean,
         formats: Array,
         editorStyle: null
     },
